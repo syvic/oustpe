e16a693ae6564e2b162d529780b17049d5d17b25
xkrambler <github@xkr.es>
2019-08-25 09:22:08 +0100

    general button short and long press support

---
+++ button.ino
@@ -0,0 +1,32 @@
+#include "button.h"
+
+byte get_button() {
+
+  static int btn_press=0;
+  static bool btn_attended=false;
+
+  if (!digitalRead(BUTTON)) {
+    if (btn_press) {
+      if (xTaskGetTickCount() - btn_press > BUTTON_TIME_LONG) {
+        if (btn_attended) {
+          return BUTTON_PRESS_NONE;
+        } else {
+          btn_attended=true;
+          return BUTTON_PRESS_LONG;
+        }
+      }
+    } else {
+      btn_press=xTaskGetTickCount();
+    }
+  } else if (btn_press) {
+    btn_press=0;
+    if (btn_attended) {
+      btn_attended=false;
+    } else {
+      return BUTTON_PRESS_SHORT;
+    }
+  }
+  return BUTTON_PRESS_NONE;
+
+}
+
